66ee094159760e9338be05facaaf98b23bc51429
rafal <rafal@openai.com>
2016-11-24 17:16:12 -0800

    add flashgames

--- envs.py
+++ envs.py
@@ -2,14 +2,74 @@ import cv2
 from gym.spaces.box import Box
 import numpy as np
 import gym
+from gym import spaces
 import logging
 import universe
 from universe import vectorized
 from universe.wrappers import BlockingReset, DiscreteToVNCAction, EpisodeID, Unvectorize, Vectorize, Vision
+from universe import spaces as vnc_spaces
+from universe.spaces.vnc_event import keycode
 import time
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 universe.configure_logging()
+
+def create_env(env_id, client_id, n=1, **kwargs):
+    spec = gym.spec(env_id)
+    remotes = "http://allocator.sci.openai-tech.com?n={}".format(n)
+
+    if spec.tags.get('flashgames', False):
+        return create_flash_env(env_id, client_id, remotes, **kwargs)
+    elif spec.tags.get('atari', False) and spec.tags.get('vnc', False):
+        return create_vncatari_env(env_id, client_id, remotes, **kwargs)
+    else:
+        # Assume atari.
+        assert "." not in env_id  # universe environments have dots in names.
+        return create_atari_env(env_id)
+
+def create_flash_env(env_id, client_id, remotes, **_):
+    env = gym.make(env_id)
+    env = Vision(env)
+    env = BlockingReset(env)
+
+    reg = universe.runtime_spec('flashgames').server_registry
+    height = reg[env_id]["height"]
+    width = reg[env_id]["width"]
+    env = CropScreen(env, height, width, 84, 18)
+
+    keys = ['left', 'right', 'up', 'down', 'n']
+    env = DiscreteToFixedKeysVNCActions(env, keys)
+    env = EpisodeID(env)
+    env = DiagnosticsInfo(env)
+    env = Unvectorize(env)
+    env.configure(fps=10.0, remotes=remotes, start_timeout=15 * 60, client_id=client_id,
+                  vnc_driver='go', vnc_kwargs={
+                    'encoding': 'tight', 'compress_level': 0,
+                    'fine_quality_level': 50, 'subsample_level': 3})
+    return env
+
+def create_vncatari_env(env_id, client_id, remotes, **_):
+    env = gym.make(env_id)
+    env = Vision(env)
+    env = BlockingReset(env)
+    env = DiscreteToVNCAction(env)
+    env = AtariRescale42x42(env)
+    env = EpisodeID(env)
+    env = DiagnosticsInfo(env)
+    env = Unvectorize(env)
+
+    logger.info('Connecting to remotes: %s', remotes)
+    fps = env.metadata['video.frames_per_second']
+    env.configure(remotes=remotes, start_timeout=15 * 60, fps=fps, client_id=client_id)
+    return env
+
+def create_atari_env(env_id):
+    env = gym.make(env_id)
+    env = Vectorize(env)
+    env = AtariRescale42x42(env)
+    env = DiagnosticsInfo(env)
+    env = Unvectorize(env)
+    return env
 
 def DiagnosticsInfo(env, *args, **kwargs):
     return vectorized.VectorizeFilter(env, DiagnosticsInfoI, *args, **kwargs)
@@ -98,20 +158,6 @@ class DiagnosticsInfoI(vectorized.Filter):
 
         return observation, reward, done, to_log
 
-def create_env(env_id, client_id, n=1, **kwargs):
-    spec = gym.spec(env_id)
-    remotes = "http://allocator.sci.openai-tech.com?n={}".format(n)
-
-    if spec.tags.get('flashgames', False):
-        return create_flash_env(env_id, client_id, remotes, **kwargs)
-    elif spec.tags.get('atari', False) and spec.tags.get('vnc', False):
-        return create_vncatari_env(env_id, client_id, remotes, **kwargs)
-    else:
-        return create_atari_env(env_id)
-
-def create_flash_env(env_id, client_id, remotes, **_):
-    raise NotImplementedError()
-
 def _process_frame42(frame):
     frame = frame[34:34+160, :160]
     frame = cv2.resize(frame, (42, 42))
@@ -129,26 +175,75 @@ class AtariRescale42x42(vectorized.ObservationWrapper):
     def _observation(self, observation_n):
         return [_process_frame42(observation) for observation in observation_n]
 
-def create_vncatari_env(env_id, client_id, remotes, **_):
-    env = gym.make(env_id)
-    assert env.metadata['runtime.vectorized']
-    env = BlockingReset(env)
-    env = DiscreteToVNCAction(env)
-    env = Vision(env)
-    env = AtariRescale42x42(env)
-    env = EpisodeID(env)
-    env = DiagnosticsInfo(env)
-    env = Unvectorize(env)
+class FixedKeyState(object):
+    def __init__(self, keys):
+        self._keys = [keycode(key) for key in keys]
+        self._down_keysyms = set()
 
-    logger.info('Connecting to remotes: %s', remotes)
-    fps = env.metadata['video.frames_per_second']
-    env.configure(remotes=remotes, start_timeout=15 * 60, fps=fps, client_id=client_id)
-    return env
+    def apply_vnc_actions(self, vnc_actions):
+        for event in vnc_actions:
+            if isinstance(event, vnc_spaces.KeyEvent):
+                if event.down:
+                    self._down_keysyms.add(event.key)
+                else:
+                    self._down_keysyms.discard(event.key)
 
-def create_atari_env(env_id):
-    env = gym.make(env_id)
-    env = Vectorize(env)
-    env = AtariRescale42x42(env)
-    env = DiagnosticsInfo(env)
-    env = Unvectorize(env)
-    return env
+    def to_index(self):
+        action_n = 0
+        for key in self._down_keysyms:
+            if key in self._keys:
+                # If multiple keys are pressed, just use the first one
+                action_n = self._keys.index(key) + 1
+                break
+        return action_n
+
+class DiscreteToFixedKeysVNCActions(vectorized.ActionWrapper):
+    def __init__(self, env, keys):
+        super(DiscreteToFixedKeysVNCActions, self).__init__(env)
+
+        self._keys = keys
+        self._generate_actions()
+        self.action_space = spaces.Discrete(len(self._actions))
+        self.key_state = FixedKeyState(keys)
+
+    def _generate_actions(self):
+        self._actions = []
+        for key in [''] + self._keys:
+            cur_action = []
+            for cur_key in self._keys:
+                cur_action.append(vnc_spaces.KeyEvent.by_name(cur_key, down=cur_key == key))
+            self._actions.append(cur_action)
+
+    def _action(self, action_n):
+        # Each action might be a length-1 np.array. Cast to int to
+        # avoid warnings.
+        return [self._actions[int(action)] for action in action_n]
+
+class CropScreen(vectorized.ObservationWrapper):
+    """Crops out a [height]x[width] area starting from (top,left) """
+    def __init__(self, env, height, width, top=0, left=0):
+        super(CropScreen, self).__init__(env)
+        self.height = height
+        self.width = width
+        self.top = top
+        self.left = left
+        self.observation_space = Box(0, 255, shape=(height, width, 3))
+
+    def _observation(self, observation_n):
+        return [ob[self.top:self.top+self.height, self.left:self.left+self.width, :] if ob is not None else None
+                for ob in observation_n]
+
+def _process_frame_flash(frame):
+    frame = cv2.resize(frame, (200, 128))
+    frame = frame.mean(2).astype(np.float32)
+    frame *= (1.0 / 255.0)
+    frame = np.reshape(frame, [128, 200, 1])
+    return frame
+
+class FlashRescale(vectorized.ObservationWrapper):
+    def __init__(self, env=None):
+        super(FlashRescale, self).__init__(env)
+        self.observation_space = Box(0, 255, [128, 200, 1])
+
+    def _observation(self, observation_n):
+        return [_process_frame_flash(observation) for observation in observation_n]
